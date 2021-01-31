94f4f36aacdaa0cb0af8c2225e8df83e012ca24d
ivan.fedorov <ivan.fedorov@demlabs.net>
2021-01-31 22:39:54 +0300

    h

--- bot2.py
+++ bot2.py
@@ -1,85 +1,73 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Jan 31 22:08:28 2021
+import logging
 
-@author: useren
-"""
-
+from aiogram import Bot, types
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
+from aiogram.dispatcher import Dispatcher
+from aiogram.dispatcher.webhook import SendMessage
+from aiogram.utils.executor import start_webhook
 import os
-from urllib.parse import urljoin
-from random import choice
 
-import aiohttp
-from aiohttp import web
-from aiogram import Bot, Dispatcher, types
-#from aiogram.utils import context
-from aiogram.dispatcher.webhook import get_new_configured_app
-from lxml import etree
 
-TOKEN = os.getenv('TOKEN', '')  # Press "Reveal Config Vars" in settings tab on Heroku and set TOKEN variable
-if not TOKEN:
+webhook_using = False
+if os.name == 'posix':
+    webhook_using = True
+    API_TOKEN = os.getenv('API_TOKEN', 'notoken')
+else:
     with open('API.TOKEN', 'r') as f:
-        TOKEN = f.readline().split()[0]
-PROJECT_NAME = os.getenv('PROJECT_NAME', '')  # Set it as you've set TOKEN env var
-if not PROJECT_NAME:
-    PROJECT_NAME = 'telegabot67'
-WEBHOOK_HOST = f'https://{PROJECT_NAME}.herokuapp.com/'  # Enter here your link from Heroku project settings
-WEBHOOK_URL_PATH = '/webhook/' + TOKEN
-WEBHOOK_URL = urljoin(WEBHOOK_HOST, WEBHOOK_URL_PATH)
-print(WEBHOOK_HOST, WEBHOOK_URL_PATH, WEBHOOK_URL, sep='\n')
-# Inline keyboard initialization with one refresh button
-inline_keyboard = types.InlineKeyboardMarkup()
-random_button = types.InlineKeyboardButton('Получить случайную цитату', callback_data='refresh')
-inline_keyboard.add(random_button)
+        API_TOKEN = f.readline().split()[0]
 
-bot = Bot(TOKEN)
+# webhook settings
+WEBHOOK_HOST = 'https://telegabot67.heroku.com'
+WEBHOOK_PATH = '/webhook/api_t'
+WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
+
+# webserver settings
+WEBAPP_HOST = '0.0.0.0'  # or ip
+WEBAPP_PORT = os.getenv('PORT', 'no_port')
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
+dp.middleware.setup(LoggingMiddleware())
 
 
-async def get_random_bash_quote():
-    """Downloads bash.im/random page, parses it and returns random quote"""
-    bash_url = 'https://bash.im/random'
+@dp.message_handler()
+async def echo(message: types.Message):
+    # Regular request
+    # await bot.send_message(message.chat.id, message.text)
 
-    async with aiohttp.ClientSession() as session:
-        async with session.get(bash_url) as resp:
-            html = await resp.text()
-
-    parser = etree.HTMLParser()
-    tree = etree.fromstring(html, parser)
-    quote_tags = tree.xpath('//div[@class="text"]')  # Xpath is a query language for selecting specific tags
-    random_quote_tag = choice(quote_tags)
-    random_quote = '\n'.join(random_quote_tag.itertext())  # The easiest way to get text inside tag divided by br tags
-    return random_quote
+    # or reply INTO webhook
+    return SendMessage(message.chat.id, message.text)
 
 
-@dp.message_handler(commands=['start'])
-async def start(message: types.Message):
-    """Handle start command"""
-    random_quote = await get_random_bash_quote()
-    await message.reply(random_quote, reply_markup=inline_keyboard)
-
-
-@dp.callback_query_handler(func=lambda cb: True)
-async def process_callback_data(callback_query: types.CallbackQuery):
-    """Handle all callback data which is being sent to bot"""
-    action = callback_query.data
-
-    if action == 'refresh':
-        message_id = callback_query.message.message_id
-        chat_id = callback_query.message.chat.id
-        random_quote = await get_random_bash_quote()
-        await bot.edit_message_text(random_quote, chat_id, message_id, reply_markup=inline_keyboard)
-
-
-async def on_startup(app):
-    """Simple hook for aiohttp application which manages webhook"""
-    await bot.delete_webhook()
+async def on_startup(dp):
     await bot.set_webhook(WEBHOOK_URL)
+    # insert code here to run it after start
+
+
+async def on_shutdown(dp):
+    logging.warning('Shutting down..')
+
+    # insert code here to run it before shutdown
+
+    # Remove webhook (not acceptable in some cases)
+    await bot.delete_webhook()
+
+    # Close DB connection (if used)
+    await dp.storage.close()
+    await dp.storage.wait_closed()
+
+    logging.warning('Bye!')
 
 
 if __name__ == '__main__':
-    # Create aiohttp.web.Application with configured route for webhook path
-    app = get_new_configured_app(dispatcher=dp, path=WEBHOOK_URL_PATH)
-    app.on_startup.append(on_startup)
-    #dp.loop.set_task_factory(context.task_factory)
-    web.run_app(app, host='0.0.0.0', port=os.getenv('PORT'))  # Heroku stores port you have to listen in your app
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        skip_updates=True,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
